df3f57891c20fb6710113fd8e7f10eee9c1830be
百墨 <shouli1990@gmail.com>
2015-11-15 12:55:50 +0800

    script

--- mice-dish/src/main/assembly/bin/start.sh
+++ mice-dish/src/main/assembly/bin/start.sh
@@ -5,33 +5,17 @@ cd ..
 DEPLOY_DIR=`pwd`
 LIB_DIR=$DEPLOY_DIR/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+CON_DIR=$DEPLOY_DIR/conf
 
-JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
-JAVA_DEBUG_OPTS=""
-if [ "$1" = "debug" ]; then
-    JAVA_DEBUG_OPTS=" -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n "
-fi
-JAVA_JMX_OPTS=""
-if [ "$1" = "jmx" ]; then
-    JAVA_JMX_OPTS=" -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false "
-fi
-JAVA_MEM_OPTS=""
-BITS=`java -version 2>&1 | grep -i 64-bit`
-if [ -n "$BITS" ]; then
-    JAVA_MEM_OPTS=" -server -Xmx2g -Xms2g -Xmn256m -XX:PermSize=256m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
-else
-    JAVA_MEM_OPTS=" -server -Xms1g -Xmx1g -XX:PermSize=2568m -XX:SurvivorRatio=2 -XX:+UseParallelGC "
-fi
+export JAVA_HOME=/export/local/jdk1.6
+export JAVA_BIN=/export/local/jdk1.6/bin
+export PATH=$JAVA_BIN:$PATH
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
-echo -e "Starting the $SERVER_NAME ...\c"
-
-nohup java $JAVA_OPTS $JAVA_MEM_OPTS $JAVA_DEBUG_OPTS $JAVA_JMX_OPTS -classpath $LIB_JARS Main &
-
+nohup java -classpath $CONF_DIR:$LIB_JARS Main &
 
 echo "OK!"
-PID=`ps -ef | grep java | grep "mic-dish" | awk '{print $2}'`
+PID=`ps -ef | grep java | grep "$mice-dish" | awk '{print $2}'`
 echo $PID > $PID_FILE
 echo "PID: $PID"
-
-
-
